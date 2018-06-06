94cdcbb1c25746485b0493eefa29fe6d8c90346d
zhangjiawei <654194944@qq.com>
2018-06-06 23:24:46 +0800

    'jQuery'

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+$(function () {  const $header = $('.header')  $(window).scroll(function () {    if ($(this).scrollTop()>71){      $header.addClass('on')    }else {      $header.removeClass('on')    }  })})
